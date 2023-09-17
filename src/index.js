c3703ab9ab97fd26e4932a715ab261a2604ba162
Opeyemi-Omotayo <Opeyemi.Motayo1@gmail.com>
2023-09-17 20:19:53 +0100

    making the data not to go stale till 5 mins

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({defaultOptions: {queries: {staleTime: 1000 * 60 *5}}});
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
