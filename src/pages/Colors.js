af2007f5ecb4fc7d56be6a617400e510049a6e1e
Opeyemi-Omotayo <Opeyemi.Motayo1@gmail.com>
2023-09-17 19:48:44 +0100

    feat; mutation

--- src/pages/Colors.js
+++ src/pages/Colors.js
@@ -1,37 +1,60 @@
-import React from 'react';
-import { useQuery } from '@tanstack/react-query';
+import React from "react";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
 const COLORS = [
-    {id:1, label: "red"},
-    {id:2, label: "yellow"},
-    {id:3, label: "blue"},
-    {id:4, label: "purple"},
-    {id:5, label: "brown"}
-]
+  { id: 1, label: "red" },
+  { id: 2, label: "yellow" },
+  { id: 3, label: "blue" },
+  { id: 4, label: "purple" },
+  { id: 5, label: "brown" },
+];
 
 const Colors = () => {
- const colorsQuery = useQuery({
-        queryKey:["colors"],
-        queryFn: () => wait(1000).then(() => [...COLORS])
-    })
+    const queryClient = useQueryClient();
 
-    if(colorsQuery.isLoading) return <h1>Loading...</h1>
-    if(colorsQuery.isError) return <h1>{JSON.stringify(colorsQuery.error)}</h1>
+  const colorsQuery = useQuery({
+    queryKey: ["colors"],
+    queryFn: () => wait(1000).then(() => [...COLORS]),
+  });
+
+  const newColorMutation = useMutation({
+    mutationFn: label => {
+        return wait(1000).then(() => {
+            COLORS.push({id: crypto.randomUUID(), label})
+        })
+    },
+    onSuccess: () => {
+        queryClient.invalidateQueries(["colors"])
+    }
+  })
+
+  if (colorsQuery.isLoading) return <h1>Loading...</h1>;
+  if (colorsQuery.isError) return <h1>{JSON.stringify(colorsQuery.error)}</h1>;
 
   return (
-    <div className='colors'>
+    <div className="colors">
       <h1>Colors</h1>
       <ul>
-        {colorsQuery.data.map(color => (
-            <li key={color.id} style={{ backgroundColor: color.label,padding:"1rem", margin:"0.5rem" }}>{color.label}</li>
+        {colorsQuery.data.map((color) => (
+          <li
+            key={color.id}
+            style={{
+              backgroundColor: color.label,
+              padding: "1rem",
+              margin: "0.5rem",
+            }}
+          >
+            {color.label}
+          </li>
         ))}
       </ul>
+      <button disabled={newColorMutation.isLoading} onClick={() => newColorMutation.mutate("green")}>Add Color</button>
     </div>
-  )
-}
+  );
+};
 
 const wait = (duration) => {
-    return new Promise(resolve => setTimeout(resolve, duration));
-}
+  return new Promise((resolve) => setTimeout(resolve, duration));
+};
 
 export default Colors;
